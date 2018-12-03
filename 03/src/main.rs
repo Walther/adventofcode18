61d14918b6bee1a91d161483251ac627f9f64b22
Veeti Haapsamo <veeti.haapsamo@gmail.com>
2018-12-02 22:45:13 -0800

    Use coordinate as tuple key

--- 03/src/main.rs
+++ 03/src/main.rs
@@ -4,7 +4,7 @@ use std::collections::HashSet;
 fn main() {
     const INPUT: &str = include_str!("input.txt");
     // 2d vector of HashSets: each square stores the ID's that use that square
-    let mut fabric: HashMap<usize, HashMap<usize, HashSet<usize>>> = HashMap::new();
+    let mut fabric: HashMap<(usize, usize), HashSet<usize>> = HashMap::new();
     let mut ids: HashSet<usize> = HashSet::new();
 
     for line in INPUT.lines() {
@@ -29,12 +29,7 @@ fn main() {
         // For each square in the defined area, insert ID in fabric
         for x in start_x..start_x + size_x {
             for y in start_y..start_y + size_y {
-                fabric
-                    .entry(x)
-                    .or_insert(HashMap::new())
-                    .entry(y)
-                    .or_insert(HashSet::new())
-                    .insert(id);
+                fabric.entry((x, y)).or_insert(HashSet::new()).insert(id);
             }
         }
     }
@@ -42,16 +37,15 @@ fn main() {
     // Count the squares that are used more than once
     // Remove overlapping ids from ids list
     let mut overlaps = 0;
-    for (_, row) in fabric {
-        for (_, square) in row {
-            if square.len() > 1 {
-                overlaps += 1;
-                for id in square {
-                    ids.remove(&id);
-                }
+    for (_, square) in fabric {
+        if square.len() > 1 {
+            overlaps += 1;
+            for id in square {
+                ids.remove(&id);
             }
         }
     }
+
     // Part 1
     println!("{}", overlaps);
 
